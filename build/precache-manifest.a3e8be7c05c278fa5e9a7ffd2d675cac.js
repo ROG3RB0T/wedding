1bff04108d73f55bd43ccd77c9ce06dbfe730f87
Roger <rogelio.menjivar@bitsoftware.dev>
2024-01-20 00:04:19 -0600

    Build to production

---
+++ build/precache-manifest.a3e8be7c05c278fa5e9a7ffd2d675cac.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6bde6b7edcad196a2f2f7fe6cf79400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5ba004e79934fa72af9",
+    "url": "/static/css/2.467c85df.chunk.css"
+  },
+  {
+    "revision": "bb2555e92d00c8600160",
+    "url": "/static/css/main.9d648170.chunk.css"
+  },
+  {
+    "revision": "a5ba004e79934fa72af9",
+    "url": "/static/js/2.10785bbf.chunk.js"
+  },
+  {
+    "revision": "bb2555e92d00c8600160",
+    "url": "/static/js/main.d7af52db.chunk.js"
+  },
+  {
+    "revision": "c5be262d17ba7045c3d3",
+    "url": "/static/js/runtime~main.88c2143d.js"
+  },
+  {
+    "revision": "2096cb3bde5621966715eae4f8e5a05f",
+    "url": "/static/media/1.2096cb3b.jpg"
+  },
+  {
+    "revision": "448f90a757178313943aa336437eeb04",
+    "url": "/static/media/1.448f90a7.jpg"
+  },
+  {
+    "revision": "9008302282fc8e1e7c7fd196b61d104b",
+    "url": "/static/media/1.90083022.jpg"
+  },
+  {
+    "revision": "fec2d0282f7744dff77f20d85aa3a170",
+    "url": "/static/media/1.fec2d028.jpg"
+  },
+  {
+    "revision": "2d7e6954096043b0e2dc152293e0aa96",
+    "url": "/static/media/4.2d7e6954.jpg"
+  },
+  {
+    "revision": "6ca6b7c524df1e59264a18015785aa7c",
+    "url": "/static/media/Amsterdam-eZvPB.6ca6b7c5.woff"
+  },
+  {
+    "revision": "0917889dd5ca03c27b3b9412557a9fdc",
+    "url": "/static/media/Flaticon.0917889d.svg"
+  },
+  {
+    "revision": "15cf01b77e18000ff8c30e5eb8132326",
+    "url": "/static/media/Flaticon.15cf01b7.woff"
+  },
+  {
+    "revision": "7ad910ab912a8296b41d09126628d1a4",
+    "url": "/static/media/Flaticon.7ad910ab.ttf"
+  },
+  {
+    "revision": "b9b714e8ebdc31809b71111250ee257e",
+    "url": "/static/media/Flaticon.b9b714e8.eot"
+  },
+  {
+    "revision": "590f17ec01bff4901c9e4c102414c2e1",
+    "url": "/static/media/b1fe2db6-e184-47fe-9654-d3ab088f99e1.590f17ec.webp"
+  },
+  {
+    "revision": "4f76942ef0c63f3bd5d271dd913f49d0",
+    "url": "/static/media/background-paper.4f76942e.jpeg"
+  },
+  {
+    "revision": "aa9cd807e9ef6bf521f7f1245212677e",
+    "url": "/static/media/cielo_abierto.aa9cd807.jpeg"
+  },
+  {
+    "revision": "123b7d37cd8d85017b593ad45c328a13",
+    "url": "/static/media/close.123b7d37.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "33973200d315530bc4dd0ddc5bf93078",
+    "url": "/static/media/hearth_1.33973200.png"
+  },
+  {
+    "revision": "ca2b84dd319246532d9eed50d2beb494",
+    "url": "/static/media/icon_1.ca2b84dd.webp"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
