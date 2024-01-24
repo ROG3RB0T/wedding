26cccc796eac56152de371ef76411c722c1bb0ee
Roger <rogelio.menjivar@bitsoftware.dev>
2024-01-23 23:51:58 -0600

    Gallery uploads

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -5,12 +5,29 @@ import Sectiontitle from '../section-title'
 import './style.css'
 
 import galimg1 from '../../images/gal/saidYes.jpeg'
-import galimg2 from '../../images/gal/J&R-47.jpg'
-import galimg3 from '../../images/gal/J&R-57.jpg'
-import galimg4 from '../../images/gal/J&R-59.jpg'
-import galimg5 from '../../images/gal/J&R-67.jpg'
-import galimg6 from '../../images/gal/J&R-93.jpg'
-import galimg7 from '../../images/gal/J&R-88.jpg'
+
+import galimg23 from '../../images/gal/J&R-23.jpg'
+import galimg24 from '../../images/gal/J&R-24.jpg'
+import galimg28 from '../../images/gal/J&R-28.jpg'
+import galimg43 from '../../images/gal/J&R-43.jpg'
+import galimg47 from '../../images/gal/J&R-47.jpg'
+import galimg51 from '../../images/gal/J&R-51.jpg'
+import galimg57 from '../../images/gal/J&R-57.jpg'
+import galimg58 from '../../images/gal/J&R-58.jpg'
+import galimg59 from '../../images/gal/J&R-59.jpg'
+import galimg67 from '../../images/gal/J&R-67.jpg'
+import galimg68 from '../../images/gal/J&R-68.jpg'
+import galimg70 from '../../images/gal/J&R-70.jpg'
+import galimg77 from '../../images/gal/J&R-77.jpg'
+import galimg80 from '../../images/gal/J&R-80.jpg'
+import galimg87 from '../../images/gal/J&R-87.jpg'
+import galimg88 from '../../images/gal/J&R-88.jpg'
+import galimg93 from '../../images/gal/J&R-93.jpg'
+import galimg94 from '../../images/gal/J&R-94.jpg'
+import galimg95 from '../../images/gal/J&R-95.jpg'
+
+
+
 
 class Gallery extends Component {
   render() {
@@ -19,8 +36,8 @@ class Gallery extends Component {
 
           <div className="container">
               <div className="row">
-                  <div className="col-lg-12 col-md-12 col-sm-12 col-12">
-                      <div className="gallery-img img-center">
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-4">
+                      <div className="gallery-img">
                         <ReactFancyBox
                             thumbnail={galimg1}
                             image={galimg1}/>
@@ -29,43 +46,127 @@ class Gallery extends Component {
                   <div className="col-lg-4 col-md-4 col-sm-4 col-4">
                       <div className="gallery-img">
                         <ReactFancyBox
-                            thumbnail={galimg2}
-                            image={galimg2}/>
+                            thumbnail={galimg23}
+                            image={galimg23}/>
                       </div>
                   </div>
                   <div className="col-lg-4 col-md-4 col-sm-4 col-4">
                       <div className="gallery-img">
                         <ReactFancyBox
-                            thumbnail={galimg3}
-                            image={galimg3}/>
+                            thumbnail={galimg24}
+                            image={galimg24}/>
                       </div>
                   </div>
                   <div className="col-lg-4 col-md-4 col-sm-4 col-4">
                       <div className="gallery-img">
                         <ReactFancyBox
-                            thumbnail={galimg4}
-                            image={galimg4}/>
+                            thumbnail={galimg28}
+                            image={galimg28}/>
                       </div>
                   </div>
                   <div className="col-lg-4 col-md-4 col-sm-4 col-4">
                       <div className="gallery-img">
                         <ReactFancyBox
-                            thumbnail={galimg5}
-                            image={galimg5}/>
+                            thumbnail={galimg43}
+                            image={galimg43}/>
                       </div>
                   </div>
                   <div className="col-lg-4 col-md-4 col-sm-4 col-4">
                       <div className="gallery-img">
                         <ReactFancyBox
-                            thumbnail={galimg6}
-                            image={galimg6}/>
+                            thumbnail={galimg47}
+                            image={galimg47}/>
                       </div>
                   </div>
                   <div className="col-lg-4 col-md-4 col-sm-4 col-4">
                       <div className="gallery-img">
                         <ReactFancyBox
-                            thumbnail={galimg7}
-                            image={galimg7}/>
+                            thumbnail={galimg51}
+                            image={galimg51}/>
+                      </div>
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-4">
+                      <div className="gallery-img">
+                        <ReactFancyBox
+                            thumbnail={galimg57}
+                            image={galimg57}/>
+                      </div>
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-4">
+                      <div className="gallery-img">
+                        <ReactFancyBox
+                            thumbnail={galimg58}
+                            image={galimg58}/>
+                      </div>
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-4">
+                      <div className="gallery-img">
+                        <ReactFancyBox
+                            thumbnail={galimg59}
+                            image={galimg59}/>
+                      </div>
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-4">
+                      <div className="gallery-img">
+                        <ReactFancyBox
+                            thumbnail={galimg67}
+                            image={galimg67}/>
+                      </div>
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-4">
+                      <div className="gallery-img">
+                        <ReactFancyBox
+                            thumbnail={galimg68}
+                            image={galimg68}/>
+                      </div>
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-4">
+                      <div className="gallery-img">
+                        <ReactFancyBox
+                            thumbnail={galimg70}
+                            image={galimg70}/>
+                      </div>
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-4">
+                      <div className="gallery-img">
+                        <ReactFancyBox
+                            thumbnail={galimg77}
+                            image={galimg77}/>
+                      </div>
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-4">
+                      <div className="gallery-img">
+                        <ReactFancyBox
+                            thumbnail={galimg80}
+                            image={galimg80}/>
+                      </div>
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-4">
+                      <div className="gallery-img">
+                        <ReactFancyBox
+                            thumbnail={galimg87}
+                            image={galimg87}/>
+                      </div>
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-4">
+                      <div className="gallery-img">
+                        <ReactFancyBox
+                            thumbnail={galimg88}
+                            image={galimg88}/>
+                      </div>
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-4">
+                      <div className="gallery-img">
+                        <ReactFancyBox
+                            thumbnail={galimg94}
+                            image={galimg94}/>
+                      </div>
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-4">
+                      <div className="gallery-img">
+                        <ReactFancyBox
+                            thumbnail={galimg95}
+                            image={galimg95}/>
                       </div>
                   </div>
               </div>
