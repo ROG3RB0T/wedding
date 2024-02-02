70dfbf5aa480610b1dcf3ea72c68b0856a3f8eeb
Roger <rogelio.menjivar@bitsoftware.dev>
2024-02-02 17:36:31 -0600

    :sparkles: Implementing redirect when no rsvp id haven't been addressed

--- src/components/rsvp/index.js
+++ src/components/rsvp/index.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import {useParams, Redirect} from 'react-router-dom';
 import { Data } from '../../resoruces/list';
 import './style.css'
 
 const Rsvp = () => {
     const { id } = useParams();
     const GuestRsvp = {id};
-    const filterObject = Data.filter(item => item.id === GuestRsvp.id);
+    const filterObject = Data.filter(item => item.id === GuestRsvp.id);   
+    if(filterObject.length < 1)
+    {
+       return <Redirect to="/home" />
+    }
     return (
             <div id="rsvp" className="rsvp-area">
                 <div className="container">
                     <div className="row">
                             <div className="rsvp-wrap">
                                     <div className="section-title section-title4 text-center">
-                                        {filterObject.map((obj,index) =>
-                                        (
-                                            <>
-                                            <h3>{obj.nombreInvitacion}</h3>
-                                            <p>Hemos reservado espacio para {obj.reserva} adulto(s).</p>
-                                            <p>Favor de confirmar su asistencia antes del 10 de Febrero, 2024 en el siguiente Link</p>
-                                            <p><a className='btn btn-success' target='_blank' href='https://docs.google.com/forms/d/e/1FAIpQLSchzVZKPEP-q76JGd2C9KaopiFCTER_lqw_3FvyLGHgnHK8xg/viewform'>Confirma Aqui</a></p>
-                                            </>
-                                        ))}                                   
+                                        {
+                                            filterObject.map((obj,index) =>
+                                            (
+                                                <>
+                                                <h3>{obj.nombreInvitacion}</h3>
+                                                <p>Hemos reservado espacio para {obj.reserva} adulto(s).</p>
+                                                <p>Favor de confirmar su asistencia antes del 10 de Febrero, 2024 en el siguiente Link</p>
+                                                <p><a className='btn btn-success' target='_blank' href='https://docs.google.com/forms/d/e/1FAIpQLSchzVZKPEP-q76JGd2C9KaopiFCTER_lqw_3FvyLGHgnHK8xg/viewform'>Confirma Aqui</a></p>
+                                                </>
+                                            ))
+                                        }                                   
                                 </div>
                                 
                             </div>
